adf3966108f88b4aff4251d0bdb0a2d808711466
wisang <wseom7@gmail.com>
2022-01-23 01:20:43 +0900

    paddle created

---
+++ main.py
@@ -0,0 +1,30 @@
+# import time
+from turtle import Screen, Turtle
+from paddle import Paddle
+
+screen = Screen()
+
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+screen.listen()
+
+paddle_r = Paddle((350, 0))
+Paddle_l = Paddle((-350, 0))
+
+screen.onkey(paddle_r.up, "Up")
+screen.onkey(paddle_r.down, "Down")
+
+screen.onkey(Paddle_l.up, "w")
+screen.onkey(Paddle_l.down, "s")
+
+# screen.tracer(0)
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+
+
+screen.exitonclick()
