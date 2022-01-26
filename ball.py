e42c0ac5849de5a28ba36e7924c47984ad7c7c1c
wisang <wseom7@gmail.com>
2022-01-26 18:28:23 +0900

    pong completed

--- ball.py
+++ ball.py
@@ -4,20 +4,30 @@ from turtle import Turtle
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
+        self.move_speed = 0.1
         self.shape("circle")
         self.color("white")
         self.width = 20
         self.height = 20
         self.penup()
-        self.goto(0,0)
-        self.y_dir = 1
+        self.goto(0, 0)
+        self.y_step = 10
+        self.x_step = 10
+
+    def bounce_y(self):
+        self.y_step *= -1
+
+    def bounce_x(self):
+        self.x_step *= -1
+        self.move_speed *= 0.9
 
     def move(self):
-        if self.ycor() > 280:
-            self.y_dir = -1
-        elif self.ycor() < -280:
-            self.y_dir = 1
+        new_x = self.xcor() + self.x_step
+        new_y = self.ycor() + self.y_step
 
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + self.y_dir * 10
         self.goto(new_x, new_y)
+
+    def reset_position(self):
+        self.bounce_x()
+        self.goto(0, 0)
+        self.move_speed = 0.1
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 
@@ -14,23 +15,43 @@ screen.tracer(0)
 screen.listen()
 
 paddle_r = Paddle((350, 0))
-Paddle_l = Paddle((-350, 0))
+paddle_l = Paddle((-350, 0))
 
 screen.onkey(paddle_r.up, "Up")
 screen.onkey(paddle_r.down, "Down")
 
-screen.onkey(Paddle_l.up, "w")
-screen.onkey(Paddle_l.down, "s")
+screen.onkey(paddle_l.up, "w")
+screen.onkey(paddle_l.down, "s")
 
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.tracer(0)
 
 is_game_on = True
+
 while is_game_on:
     screen.update()
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (ball.distance(paddle_r) < 50 and ball.xcor() > 320) \
+            or (ball.distance(paddle_l) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_score += 1
+        scoreboard.update_score()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_score += 1
+        scoreboard.update_score()
+        # is_game_on = False
 
 
 screen.exitonclick()
